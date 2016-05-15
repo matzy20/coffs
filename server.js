6b4dc701b937f8e5378e05e4ceb49324304d4886
Gail Matsushima <gail.matsushima@gmail.com>
2016-05-15 11:45:16 -1000

    installed method-override morgan passport

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+var passport = require('passport');
 
 var app = express();
 

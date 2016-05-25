8648383a67b1f9c8bdb13f8e62afe4c5c379d009
Gail Matsushima <gail.matsushima@gmail.com>
2016-05-24 19:07:42 -1000

    installed localStrategy and session

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require("express");
+var path = require("path");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
+app.use(express.static(path.resolve(__dirname, '..','public')));
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'placeholder'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res){
   console.log("Hello Coffs!");

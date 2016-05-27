8a200098d3b9e28d4fdbb30f1a0ffe5013496097
Gail Matsushima <gail.matsushima@gmail.com>
2016-05-26 22:48:36 -1000

    created User Schema via mongoose

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ var session = require('express-session');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost/Get_A_Life');
+
+var userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  created: Date,
+});
+
+var User = mongoose.model('User', userSchema);
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '..','public')));
@@ -20,6 +30,10 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+//use mongoose to create Schema
+//confirm with Jon how seed files were created in Get A Life
+//using mongoose create DB, passport, and routes
 
 app.get('/', function(req, res){
   console.log("Hello Coffs!");
